a8706b9d96223e0dbffb65ff922a5e74585e59c3
Ryan Williams <ryan.williams926@icloud.com>
2025-03-26 23:56:44 -0400

    plots for part 3, working on tooltips

--- js/main.js
+++ js/main.js
@@ -10,10 +10,37 @@ d3.csv('data/Data.csv')  //**** TO DO  switch this to loading the quakes 'data/2
     data.forEach(d => {  //convert from string to number
       d.latitude = +d.latitude; 
       d.longitude = +d.longitude;  
+
+      // force magnitude and depth to be numbers of precision 0.0
+      d.mag = +((+d.mag).toFixed(1));
+      d.depth = +((+d.depth).toFixed(0));
+
     });
 
     // Initialize chart and then show it
     leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+    // magnitude waveplot
+    wavePlot = new WavePlot({
+      parentElement: '#my-barchart',
+      barColor: 'FireBrick',
+      keyMetric: 'mag',
+      xAxisLabel: 'Quake Magnitude (converted)',
+      chartTitle: 'Distribution of Quakes by Magnitude',
+      stepSize: 0.1,
+      flipY: false
+    }, data)
+
+    // magnitude waveplot
+    wavePlot = new WavePlot({
+      parentElement: '#my-barchart',
+      barColor: 'MidnightBlue',
+      keyMetric: 'depth',
+      xAxisLabel: 'Quake Depth (km)',
+      chartTitle: 'Distribution of Quakes by Depth',
+      stepSize: 1,
+      flipY: false
+    }, data)
 
     timeline = new Timeline({ parentElement: '#my-timeline'}, data);
 
---
+++ js/wavePlot.js
@@ -0,0 +1,150 @@
+class WavePlot {
+
+	constructor(_config, _data) {
+		this.config = {
+			parentElement: _config.parentElement,
+			containerWidth: _config.containerWidth || 500,
+			containerHeight: _config.containerHeight || 500,
+			margin: { top: 40, right: 50, bottom: 80, left: 70 }
+		}
+
+		this.data = _data;
+		this.barColor = _config.barColor;
+		this.keyMetric = _config.keyMetric;
+		this.xAxisLabel = _config.xAxisLabel;
+		this.titleText = _config.chartTitle;
+        this.direction = _config.direction;
+		this.stepSize = _config.stepSize;
+		this.flipY = _config.flipY;
+
+		this.initVis();
+	}
+
+	initVis() {
+
+		let vis = this;
+
+		console.log(vis.data)
+
+		// Width and height mark the inner dimensions of the chart area
+		vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+		vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+
+		// Define size of SVG drawing area
+		vis.svg = d3.select(vis.config.parentElement).append('svg')
+			.attr('class', 'center-container')
+			.attr('width', vis.config.containerWidth)
+			.attr('height', vis.config.containerHeight);
+
+		// define chart element
+		vis.chart = vis.svg.append('g')
+			.attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
+
+		// All subsequent functions/properties can ignore the margins
+
+		// Add X axis label:
+		vis.chart.append("text")
+			.attr("text-anchor", "end")
+			.attr("x", vis.width / 2 + vis.config.margin.left + 20)
+			.attr("y", vis.height + vis.config.margin.top)
+			.text(vis.xAxisLabel);
+
+		// Y axis label:
+		vis.chart.append("text")
+			.attr("text-anchor", "end")
+			.attr("transform", "rotate(-90)")
+			.attr("y", -vis.config.margin.left + 20)
+			.attr("x", -vis.config.margin.top - 100)
+			.text("# Of Quakes")
+
+		// chart title
+		vis.chart.append("text")
+			.attr("x", (vis.width / 2))             
+			.attr("y", 0 - (vis.config.margin.top / 2))
+			.attr("text-anchor", "middle")  
+			.style("font-size", "16px") 
+			.style("text-decoration", "underline")  
+			.text(vis.titleText);
+
+		vis.updateVis()
+	}
+
+	updateVis() {
+
+		console.log("called updateVis")
+		let vis = this
+
+		// determine distribution of the current dataset (with filter applied upstream)
+		let aggrData = []
+		let xRange = d3.extent(vis.data, d => d[vis.keyMetric])
+		console.log("Current xRange: " + xRange)
+
+		let targetXValue = xRange[0]
+		while (targetXValue <= xRange[1]) {
+			aggrData.push([targetXValue, d3.filter(vis.data, (d) => d[vis.keyMetric] == targetXValue).length])
+			targetXValue += this.stepSize
+			targetXValue = +(targetXValue.toFixed(1))
+		}
+
+		console.log(aggrData)
+
+		// x scale: magnitude or depth (continuous between two extremes)
+		vis.xScale = d3.scaleLinear()
+			.domain(d3.extent(vis.data, d => d[vis.keyMetric]))
+			.range([0, vis.width]);
+		//	.paddingInner(0.15);
+
+		// y scale: # of quakes at a given converted intensity (precision 0.0)
+
+		let yRange = null
+		if (vis.flipY){
+			yRange = [0, vis.height]
+		} else {
+			yRange = [vis.height, 0]
+		}
+		vis.yScale = d3.scaleLinear()
+			.domain([0, d3.max(aggrData, d => d[1])])
+			.range(yRange);
+
+		// Initialize axes
+		const xAxis = d3.axisBottom(vis.xScale)
+		const yAxis = d3.axisLeft(vis.yScale)
+
+		// Draw the axis (move xAxis to the bottom with 'translate')
+		const xAxisGroup = vis.chart.append('g')
+			.attr('class', 'axis x-axis')
+			.attr('transform', `translate(0, ${vis.height})`)
+			.call(xAxis);
+
+		const yAxisGroup = vis.chart.append('g')
+			.attr('class', 'axis y-axis')
+			.call(yAxis);
+
+		// Add dots
+		vis.circles = vis.svg.selectAll('circle')
+			.data(aggrData)
+			.join('circle')
+			.attr('fill', vis.barColor)
+			.attr('class', 'circle')
+			.attr('cx', d => vis.xScale(d[0]) + 80)
+			.attr('cy', d => vis.yScale(d[1]) + 40)
+            .attr('r', 3);
+
+		vis.circles.on('mousemove', (event, d) => {
+			console.log("mouse over bar plot! ");
+			console.log(d);
+			console.log(event);
+
+			d3.select('#tooltip')
+				.style('opacity', 1)
+				.style('z-index', 1000000)
+				.html(`<div class="tooltip-label">
+					
+				</div>`);
+		})
+		.on('mouseleave', () => {
+			d3.select('#tooltip').style('display', 'none');
+		});
+	}
+
+}
